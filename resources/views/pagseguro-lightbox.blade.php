945d263b7cda559b54c62e309f035a7917683689
Fabiano Alves Falcão <fabiano.falcao@outlook.com>
2018-04-10 21:19:10 -0300

    Aula 11 - Melhorias no Ajax com acerto de dúvida

--- resources/views/pagseguro-lightbox.blade.php
+++ resources/views/pagseguro-lightbox.blade.php
@@ -8,26 +8,36 @@
 </head>
 <body>
     <a href="#" class="btn-buy">Finalizar compra!</a>
-
+    <div class="preloader" style="display:none">Carregando...</div>
     {!! csrf_field() !!}
+
+    
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script>
         $(function(){
             $(".btn-buy").click(function(){
-                var token = $("input[name='_token']").val();
-                $.ajax({
-                    url: "{{route('pagseguro.lightbox.code')}}",
-                    method: "POST",
-                    data: {_token: token}
-                }).done(function(code){
-                    lightbox(code);
-                }).fail(function(){
-
-                });
+                payment();
+                
                 return false;
             })
         });
+
+        function payment(){
+            startPreloader();
+            var token = $("input[name='_token']").val();
+            $.ajax({
+                url: "{{route('pagseguro.lightbox.code')}}",
+                method: "POST",
+                data: {_token: token}
+            }).done(function(code){
+                lightbox(code);
+            }).fail(function(){
+                alert("Erro inesperado.");
+            }).always(function(){
+                stopPreloader();
+            });
+        }
 
         function lightbox(code){
             var isOpenLightbox = PagSeguroLightbox({
@@ -43,6 +53,14 @@
             if(!isOpenLightbox){
                 location.href="{{config('pagseguro.url_redirect_after_request')}}"+code;
             }
+        }
+
+        function startPreloader(){
+            $('.preloader').show();
+        }
+
+        function stopPreloader(){
+            $('.preloader').hide();
         }
     </script>
     <script src="{{config('pagseguro.url_lightbox_sandbox')}}"></script>
